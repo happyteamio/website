a6f5a4f21b2ababbd67a129a4ceebb4b0d721cef
Marcin Biegała <marcin.biegala@gmail.com>
2016-04-19 20:44:45 +0200

    Add smooth scrolling to menu items

---
+++ js/document.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	        var $root = $('html, body');
+            $('a').click(function() {
+                var href = $.attr(this, 'href');
+                var offset = 0;
+                if(href !== "#") {
+                    offset = $('a[name^="' + href.substring(1) + '"]').offset().top 
+                }
+                $root.animate({
+                    scrollTop: offset
+                }, 500, function () {
+                    window.location.hash = href;
+                });
+                return false;
+            });
+        });
+
